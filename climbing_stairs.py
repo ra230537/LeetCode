901b5b10d6d12d3619e0fa03a1153e8989b00cf0
Julio Oliveira <julio.oliveira@amlabs.com.br>
2025-08-08 17:30:33 -0300

    solve climbing stars problem

---
+++ climbing_stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''Algoritmo simples, salva quantos passos leva para chegar num lugar,
+        para o proximo andar voce pode chegar partindo do anterior + 1 ou do antepenultimo +2'''
+        response = []
+            
+        for i in range(0, n):
+            # print(i)
+            # print(response)
+            if (i == 0):
+                response.append(1)
+            elif(i==1):
+                response.append(2)
+            else:
+                response.append(response[i-1]+response[i-2])
+        return response[-1]
+print(Solution().climbStairs(4))
+

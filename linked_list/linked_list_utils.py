ac3a347cffb8b375c99bab199e7e30871a584022
julio vinicius <julio.vinicius0816@gmail.com>
2025-08-17 11:07:42 -0300

    replace linked list to a specific folder

--- linked_list/linked_list_utils.py
+++ linked_list/linked_list_utils.py
@@ -12,3 +12,10 @@ def create_linked_list(array: list[int]):
         previous_node.next = current_node
         previous_node = current_node
     return head.next
+def print_linked_list_as_array(head: ListNode):
+    result = []
+    current_node = head
+    while current_node != None:
+        result.append(current_node.val)
+        current_node = current_node.next
+    print(result)

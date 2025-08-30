ce08259b7a5602572859256e46a36dc0c245504a
Julio Oliveira <julio.oliveira@amlabs.com.br>
2025-08-30 16:43:16 -0300

    fix duplicated from sorted array

--- array/remove_duplicates_from_sorted_array_2.py
+++ array/remove_duplicates_from_sorted_array_2.py
@@ -1,20 +1,22 @@
 from typing import List
+# TODO ANOTAR A IDEIA DO K QUE ANDA
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         '''
         Tu vai ter um k que representa o proximo numero diferente deveria estar
         Tu vai ter um contador porque agora a gente só vai atualizar k quando o contador for maior que 1
+        O valor na posição k deve ser atualizado sempre já que ele representa o novo vetor
+        no pior dos casos, vamos atualizar o valor de k por ele mesmo
         '''
         k  = 1
         equal_count = 0
-        for idx in range(len(nums)-1):
-            if (nums[idx] == nums[idx+1]):
+        for idx in range(1, len(nums)):
+            nums[k] = nums[idx]
+            if (nums[idx] == nums[idx-1]):
                 equal_count +=1
                 if (equal_count < 2):
                     k+=1
             else:
-                print(f'Posicao k: {k+1};Posicao idx+1: {idx+2}; Valor idx+1: {nums[idx+1]}')
-                nums[k] = nums[idx+1]
                 k+=1
                 equal_count = 0
 

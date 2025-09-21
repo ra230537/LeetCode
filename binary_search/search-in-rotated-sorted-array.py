c20ab1ca1d634228d28bd10ac1fa0ceb1a935c2d
julio vinicius <julio.vinicius0816@gmail.com>
2025-09-21 15:32:09 -0300

    refactor: remove unnecessary print statements and assertions from search in rotated sorted array

--- binary_search/search-in-rotated-sorted-array.py
+++ binary_search/search-in-rotated-sorted-array.py
@@ -56,13 +56,8 @@ class Solution:
         return max(r1, r2)
 print(Solution().search(nums = [7,8,9,0,1,2,3,4,5,6], target = 1))
 
-[8,9]
 assert Solution().search(nums = [4,5,6,7,0,1,2], target = 0) == 4
 assert Solution().search(nums = [4,5,6,7,0,1,2], target = 3) == -1
 assert Solution().search(nums = [1], target = 0) == -1
 
-
-[3,4,5,6,7,8,9,0,1,2]
-
-[7,8,9,0,1,2,3,4,5,6]
 

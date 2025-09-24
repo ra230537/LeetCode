f2a54ae624b7eb552033ac5770059850f88a2bd8
julio vinicius <julio.vinicius0816@gmail.com>
2025-09-23 21:44:31 -0300

    docs: update comments and explanations in longest increasing subsequence solution

--- dynamic_programming/longest-increasing-subsequence.py
+++ dynamic_programming/longest-increasing-subsequence.py
@@ -1,5 +1,6 @@
 from typing import List
 '''
+TODO: Anotar essa solução
 Essa questão tem uma solução que aparentemente é manjada:
 from typing import List
 import bisect
@@ -20,7 +21,16 @@ Isso é feito em O(nlog(n))
 Usando esse modulo que faz uma inserção em tempo O(log(n))
 Basicamente, a gente tem uma lista que vai conter uma subsequencia com o tamanho correto.
 Dai se o numero for adicionado no final quer dizer que achamos um numero maior para a subsequencia,então ela pode crescer
-Se o numero for adicionado no meio, substitui um candidato menor
+Se o numero for adicionado no meio, substitui por um candidato menor (o bissect_left vai dar a posição do item que fica a esquerda do target)
+Example: arr = [3, 4, 5, 1, 2, 3, 4] Let's see why keeping 1 (the smallest value) helps:
+
+We use binary search to find the position where new element is to be inserted.
+
+First three elements: buckets = [3, 4, 5]
+arr[3] = 1 buckets = [1, 4, 5]  // 1 replaces 3
+arr[4] = 2 buckets = [1, 2, 5]  // 2 replaces 4 as it's smaller
+arr[5] = 3 buckets = [1, 2, 3]  // 3 replaces 5
+arr[6] = 4 buckets = [1, 2, 3, 4]  // 4 is appended as it's larger
 '''
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
